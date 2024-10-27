7000c100c332bd76abb518b1eb63b2a40f7ae1ef
Patrycja Sawicka <patrycja.b.sawicka@gmail.com>
2024-10-27 15:56:29 +0100

    Added recomposition example

--- app/src/main/java/com/example/showcaseapplication/MainActivity.kt
+++ app/src/main/java/com/example/showcaseapplication/MainActivity.kt
@@ -12,13 +12,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.showcaseapplication.ui.theme.ShowcaseApplicationTheme
 
@@ -28,17 +35,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShowcaseApplicationTheme {
-                GreetingScreen()
+                StateHolder()
             }
         }
     }
 }
 
 @Composable
-fun GreetingScreen() {
-    // TODO: Wyświetl listę elementów pod obrazkiem
-    val items = listOf("Alice", "Bob", "Charlie", "Diana", "Ethan")
+fun StateHolder() {
+    var counter by remember { mutableStateOf(0) }
 
+    GreetingScreen(counter = counter, onIncrement = { counter++ })
+}
+
+@Composable
+fun GreetingScreen(counter: Int, onIncrement: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -50,16 +61,28 @@ fun GreetingScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { }) {
+        Text(text = "Button clicked: $counter times", color = Color.Black)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { onIncrement() }) {
             Text(text = "Click Me")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            imageVector = Icons.Default.Face,
             contentDescription = "Sample Image",
             modifier = Modifier.size(100.dp)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRecompositionDemoScreen() {
+    StateHolder()
 }
